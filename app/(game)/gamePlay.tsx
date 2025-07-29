99e49c9d8fe23200bd69d49c95604a9de4dd258c
clairox <33704909+clairox@users.noreply.github.com>
2025-07-29 10:46:25 -0400

    fix: handle undefined variable

--- app/(game)/gamePlay.tsx
+++ app/(game)/gamePlay.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from 'react-native'
+import { useGame } from '../../context/GameContext'
 
 const GamePlayPhase = () => {
+    const { letters } = useGame()
     return (
         <View>
             <Text>{letters}</Text>
