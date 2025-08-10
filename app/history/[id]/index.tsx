bc2957c4eb86c7c9956a3334321d9829b7933811
clairox <33704909+clairox@users.noreply.github.com>
2025-08-10 06:18:35 -0400

    fix(HistoryEntryPage): leave page after record deletion

--- app/history/[id]/index.tsx
+++ app/history/[id]/index.tsx
@@ -1,5 +1,5 @@
-import { Link, useLocalSearchParams } from 'expo-router'
-import { useCallback, useEffect, useState } from 'react'
+import { Link, useLocalSearchParams, useRouter } from 'expo-router'
+import { useEffect, useState } from 'react'
 import history from '../../../lib/history'
 import { FlatList, View } from 'react-native'
 import StyledText from '../../../components/ui/StyledText'
@@ -7,37 +7,38 @@ import { formatDate } from '../../../utils/helpers'
 import OverflowMenu from '../../../components/ui/OverflowMenu'
 
 const HistoryEntryPage = () => {
+    const router = useRouter()
     const { id } = useLocalSearchParams() as { id: string }
 
     const [record, setRecord] = useState<HistoryRecord | null>(null)
 
     const deleteRecord = async (): Promise<void> => {
         await history.deleteRecord(parseInt(id))
-        fetchRecord()
+        router.replace('/history')
     }
 
-    const fetchRecord = useCallback(async () => {
-        try {
-            const result = await history.getRecord(parseInt(id))
-            if (!result) {
-                throw new Error(`Record with id ${id} does not exist.`)
-            }
-
-            setRecord({
-                ...result,
-                createdAt: new Date(result.createdAt),
-                completedAt: result.completedAt
-                    ? new Date(result.completedAt)
-                    : null,
-            })
-        } catch (err) {
-            throw new Error(`An unexpected error has occurred: ${err}`)
-        }
-    }, [id])
-
     useEffect(() => {
+        const fetchRecord = async () => {
+            try {
+                const result = await history.getRecord(parseInt(id))
+                if (!result) {
+                    throw new Error(`Record with id ${id} does not exist.`)
+                }
+
+                setRecord({
+                    ...result,
+                    createdAt: new Date(result.createdAt),
+                    completedAt: result.completedAt
+                        ? new Date(result.completedAt)
+                        : null,
+                })
+            } catch (err) {
+                throw new Error(`An unexpected error has occurred: ${err}`)
+            }
+        }
+
         fetchRecord()
-    }, [fetchRecord])
+    }, [id])
 
     if (record) {
         return (
